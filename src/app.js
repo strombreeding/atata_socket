07cc5d902ca47c1862263f16a5d77a142833dfe6
strombreeding <speaker1403@gmail.com>
2024-03-29 02:53:20 +0900

    ㅇㅇ

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import http from "http";
 import { Server as SocketServer } from "socket.io";
 import cors from "cors";
 import { randomUUID } from "crypto";
+import axios from "axios";
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -15,29 +16,29 @@ app.get("/", (req, res) => {
   return res.send("hello world");
 });
 let waitingUsers = [];
-/* 
-  아래 타입
-  {
-  mid : string;
-  user1: User;
-  user2: User;
-  stats : "boom" | "success"
-}[]
-*/
+
+const waitPropsList = [];
 let matchingRoom = []; //
 // 클라이언트가 연결되었을 때 실행되는 이벤트 리스너
 io.on("connection", (socket) => {
-  socket.on("joinQueue", () => {
+  socket.on("joinQueue", async ({ id, nickname }) => {
+    const res = await axios.get("http://localhost:8080/user/one/" + id);
     waitingUsers.push(socket);
+    waitPropsList.push({
+      nickname: res.data.nickname,
+      bounti: res.data.bounti,
+    });
     if (waitingUsers.length >= 2) {
       // Matchmaking
       const user1 = waitingUsers.shift();
       const user2 = waitingUsers.shift();
+      const userProp1 = waitPropsList.shift();
+      const userProp2 = waitPropsList.shift();
       const roomId = randomUUID();
       matchingRoom.push(roomId);
       // Emit message to users for connection
-      user1.emit("matchFound", roomId);
-      user2.emit("matchFound", roomId);
+      user1.emit("matchFound", { roomId, targetProps: userProp2 });
+      user2.emit("matchFound", { roomId, targetProps: userProp1 });
     }
   });
 
@@ -46,12 +47,10 @@ io.on("connection", (socket) => {
     const idx = matchingRoom.indexOf(id);
     socket.join(id);
     const acceptCnt = io.sockets.adapter.rooms.get(id);
-    if (acceptCnt && acceptCnt.size >= 2) {
-      // ㅇㅋ 둘다 수락 이동해.
-      socket.to(id).emit("welcome");
-      socket.to(id).emit("goBang");
-      socket.emit("goBang"); // 이동하라는 emit 보내기
-    } else if (idx < 0) {
+    console.log(acceptCnt);
+    console.log(acceptCnt.size);
+    console.log(idx);
+    if (idx < 0) {
       socket.emit("cancelMatch");
       // socket.rooms.delete(id);
       io.sockets.adapter.rooms.delete(id);
@@ -59,6 +58,11 @@ io.on("connection", (socket) => {
         "유저가 취소하거나 이동함 ",
         io.sockets.adapter.rooms.get(id)
       );
+    } else if (acceptCnt && acceptCnt.size >= 2) {
+      // ㅇㅋ 둘다 수락 이동해.
+      socket.to(id).emit("welcome");
+      socket.to(id).emit("goBang");
+      socket.emit("goBang"); // 이동하라는 emit 보내기
     }
   });
 
@@ -78,6 +82,8 @@ io.on("connection", (socket) => {
     socket.to(id).emit("cancelMatch");
     matchingRoom.splice(idx, 1);
     socket.rooms.delete(id);
+    io.sockets.adapter.rooms.get(id);
+
     // waitingUsers.push(socket);
   });
 
